322194aeba1025c8a0602c6fe0cd9b643ccfec47
Valan Aravind <valz1711@gmail.com>
2019-12-31 23:45:49 +0800

    Spark Basics initial commit

---
+++ PySparkBasics/SparkDataFrames.py
@@ -0,0 +1,8 @@
+from pyspark.sql import SparkSession
+print("New Year")
+if(__name__ == '__main__'):
+    spark = SparkSession.builder.getOrCreate()
+    path = "C:/Users/Valan Aravind/Desktop/Scala/R/Data/SparkCSV.csv"
+    sparkDF = spark.read.csv(path,header=True)
+
+    sparkDF.show()

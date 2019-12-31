384ca7313a94d329ed5f05ce0d9f81a280d236ba
Valan Aravind <valz1711@gmail.com>
2020-01-01 00:07:17 +0800

    Update SparkDataFrames.py
    Make corrections accordingly

--- PySparkBasics/SparkDataFrames.py
+++ PySparkBasics/SparkDataFrames.py
@@ -4,6 +4,6 @@ if(__name__ == '__main__'):
     spark = SparkSession.builder.getOrCreate()
     path = "C:/Users/Valan Aravind/Desktop/Scala/R/Data/SparkCSV.csv"
     sparkDF = spark.read.csv(path,header=True)
-
+    print("Please order by Sl_no")
     sparkDF.show()
     print("Spark dataframe is succesfully displayed")
